a434a44709da0e7d6df0aa097937f203af15feb8
Vishal Chand <sam.chand96@gmail.com>
2023-05-14 15:53:03 +0530

    Chapter 3 complete.

--- lib/main.dart
+++ lib/main.dart
@@ -17,28 +17,32 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Awesome")
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          height: 100,
-          width: 100,
-          alignment: Alignment.center,
-          decoration:  BoxDecoration(
-            gradient: const LinearGradient(colors: [Colors.yellow, Colors.pink]),
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: const [
-              BoxShadow(
-              color: Colors.grey,
-              blurRadius: 5,
-              offset: Offset(2.0, 5.0)
-            )
-            ]
+      body: Align(
+        alignment: Alignment.bottomRight,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: 200,
+            height: 400,
+            color: Colors.black,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [ Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+              ), Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+              ), Container(
+                height: 100,
+                width: 100,
+                color: Colors.yellow,
+              )],
+            ),
           ),
-          child: const Text("I am a box",textAlign: TextAlign.center, style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 20
-          ),),
         ),
       )
     );
