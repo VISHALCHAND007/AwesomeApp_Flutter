a75c5165e5e508708ea451688167fa81d2525c8a
Vishal Chand <sam.chand96@gmail.com>
2023-05-14 15:34:17 +0530

    Chapter 2 complete.

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,30 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Awesome")
       ),
-      body: const Center(child: Text("Hello World.")),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          height: 100,
+          width: 100,
+          alignment: Alignment.center,
+          decoration:  BoxDecoration(
+            gradient: const LinearGradient(colors: [Colors.yellow, Colors.pink]),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: const [
+              BoxShadow(
+              color: Colors.grey,
+              blurRadius: 5,
+              offset: Offset(2.0, 5.0)
+            )
+            ]
+          ),
+          child: const Text("I am a box",textAlign: TextAlign.center, style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20
+          ),),
+        ),
+      )
     );
   }
 }
